702ba2318be3802d63fc2bc08ba95731683f64f4
robosaz <shahin.xt8aj@gmail.com>
2017-06-25 21:20:49 +0430

    Update s1.lua

--- plugins/s1.lua
+++ plugins/s1.lua
@@ -1,18 +1,18 @@
 local function run(msg, matches)
-  if matches[1] == "دستورات فارسی" then
+  if matches[1] == "فارسی" then
     local file = matches[2]
     if is_sudo(msg) then --sudo only !
       local receiver = get_receiver(msg)
       send_document(receiver, "./infoXT/text/HelpXt.sh", ok_cb, false)
       else
-      return "شما مجاز به استفاده از این استیکر نیستید"
+      return "شما مجاز به استفاده از این پلاگین نیستید"
     end
   end
 end
 
 return {
   patterns = {
-  "^(دستورات فارسی) (.*)$"
+  "^(فارسی) (.*)$"
   },
   run = run
 }
