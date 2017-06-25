7ff33912417cf7017daac4e009febbae9f578d5f
robosaz <shahin.xt8aj@gmail.com>
2017-06-25 22:48:01 +0430

    Update s2.lua

--- plugins/s2.lua
+++ plugins/s2.lua
@@ -1,9 +1,9 @@
 local function run(msg, matches)
-  if matches[1] == "help-fa" then
+  if matches[1] == "سازنده" then
     local file = matches[2]
     if is_sudo(msg) then --sudo only !
       local receiver = get_receiver(msg)
-      send_document(receiver, "./infoXT/text/HelpXt1.sh", ok_cb, false)
+      send_document(receiver, "./plugins/cc.webp", ok_cb, false)
       else
       return "شما مجاز به استفاده از این پلاگین نیستید"
     end
@@ -12,7 +12,7 @@ end
 
 return {
   patterns = {
-  "^(help-fa) (.*)$"
+  "^(سازنده) (.*)$"
   },
   run = run
 }
